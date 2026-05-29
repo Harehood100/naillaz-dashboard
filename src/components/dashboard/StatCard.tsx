cf529377c79c6781c94bc4cd465302dae27b7a8f
Harehood100 <bolajibakare498@gmail.com>
2026-05-30 00:52:52 +0100

    Update StatCard.tsx

--- src/components/dashboard/StatCard.tsx
+++ src/components/dashboard/StatCard.tsx
@@ -1,1 +1,16 @@
+type StatCardProps = {
+  title: string;
+  amount: string;
+};
 
+export default function StatCard({
+  title,
+  amount,
+}: StatCardProps) {
+  return (
+    <div className="stat-card">
+      <h4>{title}</h4>
+      <h2>{amount}</h2>
+    </div>
+  );
+}
